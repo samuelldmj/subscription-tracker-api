86e0d027a242e00fc282df34272b98bead6e4584
Samuel <samuelldmj5@gmail.com>
2026-04-12 19:33:40 +0100

    feat(user): add tokenVersion field to schema
    Introduce a numeric tokenVersion field with default value 0 to support authentication token versioning and invalidation mechanisms.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -29,6 +29,10 @@ const userSchema = new mongoose.Schema(
             default: "UTC",
             trim: true,
         },
+        tokenVersion: {
+            type: Number,
+            default: 0
+        }
     },
     { timestamps: true }
 );
